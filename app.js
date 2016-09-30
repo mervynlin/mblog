640c56e447216ef84e865cd8453308a95aaefcea
mervynlin <linhuiman@gmail.com>
2016-09-30 13:16:35 +0800

    changed comment

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Change some comment
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
